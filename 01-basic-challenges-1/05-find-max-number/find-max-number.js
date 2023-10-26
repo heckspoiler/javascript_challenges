b861f6f8bb444e1059ac3b7a543f552a5a31b0f4
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-26 16:22:33 +0200

    fifth finished

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let num;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= arr[i - 1]) {
+      num = arr[i];
+    }
+  }
+  console.log(num);
+  return num;
+}
 
 module.exports = findMaxNumber;
+
+// other solution:
+// function findMaxNumber(arr){
+// return Math.max(..arr);
+//}
