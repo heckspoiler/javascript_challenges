f92ebe55abcf9cea73ed82969364895190b0e83f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-26 16:13:51 +0200

    third challenge finished

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+function countOccurrences(str, letter) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countOccurrences;
+
+// other option
+
+// const countOccurences = (str, char) => str.split((char).length - 1)
