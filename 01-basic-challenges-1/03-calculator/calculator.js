36d4b1eb3f01df7788f5b7d4d44337e476ebafef
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-26 15:34:44 +0200

    first three challenges managed

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,24 @@
-function calculator() {}
+function calculator(a, b, operator) {
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = a + b;
+      break;
+    case '-':
+      result = a - b;
+      break;
+    case '*':
+      result = a * b;
+      break;
+    case '/':
+      result = a / b;
+      break;
+    default:
+      console.log(Error);
+  }
+
+  return result;
+}
 
 module.exports = calculator;
